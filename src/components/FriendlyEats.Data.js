177383a40b3d377d5a77d42e01a56a2dc379c0d3
Isamu Arimoto <isamu@to-kyo.to>
2019-08-05 13:00:13 +0900

    add getDocumentsInQuery

--- src/components/FriendlyEats.Data.js
+++ src/components/FriendlyEats.Data.js
@@ -5,31 +5,37 @@ export const addRestaurant = (data) => {
   /*
     TODO: Implement adding a document
   */
-}
+};
 
 export const getAllRestaurants = () => {
   /*
     TODO: Retrieve list of restaurants
   */
-}
+};
+
+export const getDocumentsInQuery = (query, renderer) => {
+  /*
+    TODO: Render all documents in the provided query
+  */
+};
 
 export const getRestaurant = (id) => {
   /*
     TODO: Retrieve a single restaurant
   */
-}
+};
 
 export const getFilteredRestaurants = (filters) => {
   /*
     TODO: Retrieve filtered list of restaurants
   */
-}
+};
 
 export const addRating = (restaurantID, rating) => {
   /*
     TODO: Retrieve add a rating to a restaurant
   */
-}
+};
 
 export const getRating = (id) => {
   return firebase.firestore().collection('restaurants').doc(id).collection('ratings').orderBy('timestamp', 'desc');
